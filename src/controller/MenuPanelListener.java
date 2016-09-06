a7387c24d690bd0e6f6ee4e34200e067f7bead47
ibpop <blazejpop@wp.pl>
2016-09-06 19:31:58 +0200

    chyba dziala

--- src/controller/MenuPanelListener.java
+++ src/controller/MenuPanelListener.java
@@ -9,16 +9,17 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MenuPanelListener implements ActionListener{
+public class MenuPanelListener implements ActionListener {
+
     private static MainFrame mainFrame;
-    private static MenuPanelListener menuListener =  new MenuPanelListener();
+    private static MenuPanelListener menuListener = new MenuPanelListener();
     private static PlayerContainer playerContainer;
 
-    private MenuPanelListener(){
+    private MenuPanelListener() {
 
     }
 
-    public static MenuPanelListener getInstance(){
+    public static MenuPanelListener getInstance() {
         return menuListener;
     }
 
@@ -26,26 +27,27 @@ public class MenuPanelListener implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
 
-        if(mainFrame == null) {
+        if (mainFrame == null) {
             mainFrame = MainFrame.getInstance();
         }
 
-        if(playerContainer == null){
+        if (playerContainer == null) {
             playerContainer = PlayerContainer.getInstance();
         }
-        
-        if(source instanceof JMenuItem){
+
+        if (source instanceof JMenuItem) {
             String buttonLabel = ((JMenuItem) source).getName();
-            
-            if(buttonLabel.equals("MENU_CLOSE"))
+
+            if (buttonLabel.equals("MENU_CLOSE")) {
                 System.exit(0);
+            }
         }
 
-        if(source instanceof JButton){
+        if (source instanceof JButton) {
             String buttonLabel = ((JButton) source).getName();
             //zmienic na enum !!! i wrzucic do model
 
-            switch (buttonLabel){
+            switch (buttonLabel) {
                 case "NEW_GAME":
                     mainFrame.setPlayerPanel();
                     break;
@@ -55,7 +57,10 @@ public class MenuPanelListener implements ActionListener{
                     PlayerContainer.GameMode gameMode = mainFrame.getGameMode();
                     playerContainer.addPlayer(new Player(playerName));
 
-                    switch (gameMode){
+                    int shipsLeft = playerContainer.getCurrentPlayer().getNumberOfShipsToSet();
+                    mainFrame.getSpaceShipsPanel().setShipsLeftLabel(shipsLeft);
+
+                    switch (gameMode) {
                         case VS_COMPUTER:
                             playerContainer.addPlayer(new ComputerPlayer("komputer"));
                             break;
